11cd6c97cd2a2812e12fc71ca29f295aaba02008
Yu Jin Choe <forworkchoe@gmail.com>
2025-12-01 22:51:02 +0900

    어차피 true나 false 한쪽으로 고정되는 온갖 props를 전부 삭제

--- packages/mui-material/src/Modal/Modal.js
+++ packages/mui-material/src/Modal/Modal.js
@@ -76,31 +76,27 @@ const ModalBackdrop = styled(Backdrop, {
 const Modal = React.forwardRef(function Modal(inProps, ref) {
   const props = useDefaultProps({ name: 'MuiModal', props: inProps });
   const {
-    BackdropProps,
+    children,
     classes: classesProp,
     className,
-    closeAfterTransition = false,
-    children,
     container,
-    component,
-    components = {},
-    componentsProps = {},
-    disableAutoFocus = false,
-    disableEnforceFocus = false,
-    disableEscapeKeyDown = false,
-    disablePortal = false,
-    disableRestoreFocus = false,
-    keepMounted = false,
     onClose,
-    onTransitionEnter,
-    onTransitionExited,
     open,
-    slotProps = {},
-    slots = {},
     // eslint-disable-next-line react/prop-types
     theme,
     ...other
   } = props;
+
+  // 하드코딩된 기본값들
+  const closeAfterTransition = false;
+  const disableAutoFocus = false;
+  const disableEnforceFocus = false;
+  const disableEscapeKeyDown = false;
+  const disablePortal = false;
+  const disableRestoreFocus = false;
+  const keepMounted = false;
+  const disableScrollLock = false;
+  const hideBackdrop = false;
 
   const propsWithDefaults = {
     ...props,
@@ -111,6 +107,8 @@ const Modal = React.forwardRef(function Modal(inProps, ref) {
     disablePortal,
     disableRestoreFocus,
     keepMounted,
+    disableScrollLock,
+    hideBackdrop,
   };
 
   const {
@@ -145,25 +143,11 @@ const Modal = React.forwardRef(function Modal(inProps, ref) {
     childProps.onExited = onExited;
   }
 
-  const externalForwardedProps = {
-    slots: {
-      root: components.Root,
-      backdrop: components.Backdrop,
-      ...slots,
-    },
-    slotProps: {
-      ...componentsProps,
-      ...slotProps,
-    },
-  };
-
   const [RootSlot, rootProps] = useSlot('root', {
     ref,
     elementType: ModalRoot,
     externalForwardedProps: {
-      ...externalForwardedProps,
       ...other,
-      component,
     },
     getSlotProps: getRootProps,
     ownerState,
@@ -175,11 +159,8 @@ const Modal = React.forwardRef(function Modal(inProps, ref) {
   });
 
   const [BackdropSlot, backdropProps] = useSlot('backdrop', {
-    ref: BackdropProps?.ref,
     elementType: ModalBackdrop,
-    externalForwardedProps,
     shouldForwardComponentProp: true,
-    additionalProps: BackdropProps,
     getSlotProps: (otherHandlers) => {
       return getBackdropProps({
         ...otherHandlers,
@@ -190,7 +171,7 @@ const Modal = React.forwardRef(function Modal(inProps, ref) {
         },
       });
     },
-    className: clsx(BackdropProps?.className, classes?.backdrop),
+    className: clsx(classes?.backdrop),
     ownerState,
   });
 
@@ -222,23 +203,6 @@ Modal.propTypes /* remove-proptypes */ = {
   // │    To update them, edit the d.ts file and run `pnpm proptypes`.     │
   // └─────────────────────────────────────────────────────────────────────┘
   /**
-   * A backdrop component. This prop enables custom backdrop rendering.
-   * @deprecated Use `slots.backdrop` instead. While this prop currently works, it will be removed in the next major version.
-   * Use the `slots.backdrop` prop to make your application ready for the next version of Material UI.
-   * @default styled(Backdrop, {
-   *   name: 'MuiModal',
-   *   slot: 'Backdrop',
-   * })({
-   *   zIndex: -1,
-   * })
-   */
-  BackdropComponent: PropTypes.elementType,
-  /**
-   * Props applied to the [`Backdrop`](https://mui.com/material-ui/api/backdrop/) element.
-   * @deprecated Use `slotProps.backdrop` instead.
-   */
-  BackdropProps: PropTypes.object,
-  /**
    * A single child content element.
    */
   children: elementAcceptingRef.isRequired,
@@ -250,39 +214,6 @@ Modal.propTypes /* remove-proptypes */ = {
    * @ignore
    */
   className: PropTypes.string,
-  /**
-   * When set to true the Modal waits until a nested Transition is completed before closing.
-   * @default false
-   */
-  closeAfterTransition: PropTypes.bool,
-  /**
-   * The component used for the root node.
-   * Either a string to use a HTML element or a component.
-   */
-  component: PropTypes.elementType,
-  /**
-   * The components used for each slot inside.
-   *
-   * @deprecated Use the `slots` prop instead. This prop will be removed in a future major release. See [Migrating from deprecated APIs](https://mui.com/material-ui/migration/migrating-from-deprecated-apis/) for more details.
-   *
-   * @default {}
-   */
-  components: PropTypes.shape({
-    Backdrop: PropTypes.elementType,
-    Root: PropTypes.elementType,
-  }),
-  /**
-   * The extra props for the slot components.
-   * You can override the existing props or add new ones.
-   *
-   * @deprecated Use the `slotProps` prop instead. This prop will be removed in a future major release. See [Migrating from deprecated APIs](https://mui.com/material-ui/migration/migrating-from-deprecated-apis/) for more details.
-   *
-   * @default {}
-   */
-  componentsProps: PropTypes.shape({
-    backdrop: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-    root: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-  }),
   /**
    * An HTML element or function that returns one.
    * The `container` will have the portal children appended to it.
@@ -298,52 +229,6 @@ Modal.propTypes /* remove-proptypes */ = {
     PropTypes.func,
   ]),
   /**
-   * If `true`, the modal will not automatically shift focus to itself when it opens, and
-   * replace it to the last focused element when it closes.
-   * This also works correctly with any modal children that have the `disableAutoFocus` prop.
-   *
-   * Generally this should never be set to `true` as it makes the modal less
-   * accessible to assistive technologies, like screen readers.
-   * @default false
-   */
-  disableAutoFocus: PropTypes.bool,
-  /**
-   * If `true`, the modal will not prevent focus from leaving the modal while open.
-   *
-   * Generally this should never be set to `true` as it makes the modal less
-   * accessible to assistive technologies, like screen readers.
-   * @default false
-   */
-  disableEnforceFocus: PropTypes.bool,
-  /**
-   * If `true`, hitting escape will not fire the `onClose` callback.
-   * @default false
-   */
-  disableEscapeKeyDown: PropTypes.bool,
-  /**
-   * The `children` will be under the DOM hierarchy of the parent component.
-   * @default false
-   */
-  disablePortal: PropTypes.bool,
-  /**
-   * If `true`, the modal will not restore focus to previously focused element once
-   * modal is hidden or unmounted.
-   * @default false
-   */
-  disableRestoreFocus: PropTypes.bool,
-  /**
-   * If `true`, the backdrop is not rendered.
-   * @default false
-   */
-  hideBackdrop: PropTypes.bool,
-  /**
-   * Always keep the children in the DOM.
-   * This prop can be useful in SEO situation or
-   * when you want to maximize the responsiveness of the Modal.
-   * @default false
-   */
-  keepMounted: PropTypes.bool,
-  /**
    * Callback fired when the component requests to be closed.
    * The `reason` parameter can optionally be used to control the response to `onClose`.
    *
@@ -352,34 +237,9 @@ Modal.propTypes /* remove-proptypes */ = {
    */
   onClose: PropTypes.func,
   /**
-   * A function called when a transition enters.
-   */
-  onTransitionEnter: PropTypes.func,
-  /**
-   * A function called when a transition has exited.
-   */
-  onTransitionExited: PropTypes.func,
-  /**
    * If `true`, the component is shown.
    */
   open: PropTypes.bool.isRequired,
-  /**
-   * The props used for each slot inside the Modal.
-   * @default {}
-   */
-  slotProps: PropTypes.shape({
-    backdrop: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-    root: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-  }),
-  /**
-   * The components used for each slot inside the Modal.
-   * Either a string to use a HTML element or a component.
-   * @default {}
-   */
-  slots: PropTypes.shape({
-    backdrop: PropTypes.elementType,
-    root: PropTypes.elementType,
-  }),
   /**
    * The system prop that allows defining system overrides as well as additional CSS styles.
    */
