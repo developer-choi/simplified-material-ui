e204177dad7e7e39b6400ab027247c636494875a
Yu Jin Choe <forworkchoe@gmail.com>
2025-12-02 20:53:08 +0900

    Dialog.js에 DialogContainer 및 DialogPaper 컴포넌트 재도입

--- packages/mui-material/src/Dialog/Dialog.js
+++ packages/mui-material/src/Dialog/Dialog.js
@@ -4,6 +4,55 @@ import useId from '@mui/utils/useId';
 import Modal from '../Modal';
 import DialogContext from './DialogContext';
 
+const DialogContainer = React.forwardRef(function DialogContainer(props, ref) {
+  const { children, style, ...other } = props;
+  return (
+    <div
+      ref={ref}
+      role="presentation"
+      {...other}
+      style={{
+        height: '100%',
+        outline: 0,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        ...style,
+      }}
+    >
+      {children}
+    </div>
+  );
+});
+
+const DialogPaper = React.forwardRef(function DialogPaper(props, ref) {
+  const { children, style, ...other } = props;
+  return (
+    <div
+      ref={ref}
+      role="dialog"
+      aria-modal="true"
+      {...other}
+      style={{
+        margin: 32,
+        position: 'relative',
+        overflowY: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        maxHeight: 'calc(100% - 64px)',
+        maxWidth: '600px',
+        width: 'calc(100% - 64px)',
+        backgroundColor: '#fff',
+        borderRadius: 4,
+        boxShadow: '0px 11px 15px -7px rgba(0,0,0,0.2),0px 24px 38px 3px rgba(0,0,0,0.14),0px 9px 46px 8px rgba(0,0,0,0.12)',
+        ...style,
+      }}
+    >
+      {children}
+    </div>
+  );
+});
+
 /**
  * Dialogs are overlaid modal paper based components with a backdrop.
  */
@@ -11,7 +60,6 @@ const Dialog = React.forwardRef(function Dialog(inProps, ref) {
   const {
     'aria-describedby': ariaDescribedby,
     'aria-labelledby': ariaLabelledbyProp,
-    'aria-modal': ariaModal = true,
     children,
     className,
     disableEscapeKeyDown = false,
@@ -34,38 +82,14 @@ const Dialog = React.forwardRef(function Dialog(inProps, ref) {
       open={open}
       {...other}
     >
-      <div
-        role="presentation"
-        style={{
-          height: '100%',
-          outline: 0,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <div
-          role="dialog"
+      <DialogContainer>
+        <DialogPaper
           aria-describedby={ariaDescribedby}
           aria-labelledby={ariaLabelledby}
-          aria-modal={ariaModal}
-          style={{
-            margin: 32,
-            position: 'relative',
-            overflowY: 'auto',
-            display: 'flex',
-            flexDirection: 'column',
-            maxHeight: 'calc(100% - 64px)',
-            maxWidth: '600px',
-            width: 'calc(100% - 64px)',
-            backgroundColor: '#fff',
-            borderRadius: 4,
-            boxShadow: '0px 11px 15px -7px rgba(0,0,0,0.2),0px 24px 38px 3px rgba(0,0,0,0.14),0px 9px 46px 8px rgba(0,0,0,0.12)',
-          }}
         >
           <DialogContext.Provider value={dialogContextValue}>{children}</DialogContext.Provider>
-        </div>
-      </div>
+        </DialogPaper>
+      </DialogContainer>
     </Modal>
   );
 });
