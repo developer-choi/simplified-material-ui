310770cbdb0420617cc5f5a71672a3e9c716f687
Yu Jin Choe <forworkchoe@gmail.com>
2025-12-07 15:42:39 +0900

    Modal 구조 단순화 및 스타일 의존성 제거

--- packages/mui-material/src/Modal/Modal.js
+++ packages/mui-material/src/Modal/Modal.js
@@ -1,49 +1,8 @@
 'use client';
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import HTMLElementType from '@mui/utils/HTMLElementType';
-import elementAcceptingRef from '@mui/utils/elementAcceptingRef';
 import FocusTrap from '../Unstable_TrapFocus';
 import Portal from '../Portal';
-import { styled } from '../zero-styled';
-import memoTheme from '../utils/memoTheme';
-import Backdrop from '../Backdrop';
 import useModal from './useModal';
-
-const ModalRoot = styled('div', {
-  name: 'MuiModal',
-  slot: 'Root',
-  overridesResolver: (props, styles) => {
-    const { ownerState } = props;
-
-    return [styles.root, !ownerState.open && ownerState.exited && styles.hidden];
-  },
-})(
-  memoTheme(({ theme }) => ({
-    position: 'fixed',
-    zIndex: (theme.vars || theme).zIndex.modal,
-    right: 0,
-    bottom: 0,
-    top: 0,
-    left: 0,
-    variants: [
-      {
-        props: ({ ownerState }) => !ownerState.open && ownerState.exited,
-        style: {
-          visibility: 'hidden',
-        },
-      },
-    ],
-  })),
-);
-
-const ModalBackdrop = styled(Backdrop, {
-  name: 'MuiModal',
-  slot: 'Backdrop',
-})({
-  zIndex: -1,
-});
 
 /**
  * Modal is a lower-level construct that is leveraged by the following components:
@@ -62,12 +21,10 @@ const Modal = React.forwardRef(function Modal(inProps, ref) {
   const props = inProps;
   const {
     children,
-    classes,
     className,
     container,
     onClose,
     open,
-    theme,
     ...other
   } = props;
 
@@ -104,11 +61,6 @@ const Modal = React.forwardRef(function Modal(inProps, ref) {
     rootRef: ref,
   });
 
-  const ownerState = {
-    ...propsWithDefaults,
-    exited,
-  };
-
   const childProps = {};
   if (children.props.tabIndex === undefined) {
     childProps.tabIndex = '-1';
@@ -125,22 +77,36 @@ const Modal = React.forwardRef(function Modal(inProps, ref) {
 
   return (
     <Portal ref={portalRef} container={container} disablePortal={disablePortal}>
-      {/* RootSlot 대신 ModalRoot 직접 사용 */}
-      <ModalRoot
+      {/* RootSlot 대신 div 직접 사용 */}
+      <div
         {...rootProps}
-        className={clsx(
-          className,
-          rootProps.className
-        )}
-        ownerState={ownerState}
+        className={className}
+        style={{
+          position: 'fixed',
+          zIndex: 1300,
+          right: 0,
+          bottom: 0,
+          top: 0,
+          left: 0,
+          visibility: !open && exited ? 'hidden' : undefined,
+          ...rootProps.style,
+        }}
       >
-        {/* BackdropSlot 대신 ModalBackdrop 직접 사용 */}
+        {/* BackdropSlot 대신 div 직접 사용 */}
         {!hideBackdrop && (
-           <ModalBackdrop
+           <div
              {...backdropProps}
-             className={clsx(backdropProps.className)}
-             open={open}
-             ownerState={ownerState}
+             style={{
+               zIndex: -1,
+               position: 'fixed',
+               right: 0,
+               bottom: 0,
+               top: 0,
+               left: 0,
+               backgroundColor: 'rgba(0, 0, 0, 0.5)',
+               WebkitTapHighlightColor: 'transparent',
+               ...backdropProps.style,
+             }}
            />
         )}
 
@@ -153,62 +119,9 @@ const Modal = React.forwardRef(function Modal(inProps, ref) {
         >
           {React.cloneElement(children, childProps)}
         </FocusTrap>
-      </ModalRoot>
+      </div>
     </Portal>
   );
 });
-
-Modal.propTypes /* remove-proptypes */ = {
-  // ┌────────────────────────────── Warning ──────────────────────────────┐
-  // │ These PropTypes are generated from the TypeScript type definitions. │
-  // │    To update them, edit the d.ts file and run `pnpm proptypes`.     │
-  // └─────────────────────────────────────────────────────────────────────┘
-  /**
-   * A single child content element.
-   */
-  children: elementAcceptingRef.isRequired,
-  /**
-   * Override or extend the styles applied to the component.
-   */
-  classes: PropTypes.object,
-  /**
-   * @ignore
-   */
-  className: PropTypes.string,
-  /**
-   * An HTML element or function that returns one.
-   * The `container` will have the portal children appended to it.
-   *
-   * You can also provide a callback, which is called in a React layout effect.
-   * This lets you set the container from a ref, and also makes server-side rendering possible.
-   *
-   * By default, it uses the body of the top-level document object,
-   * so it's simply `document.body` most of the time.
-   */
-  container: PropTypes /* @typescript-to-proptypes-ignore */.oneOfType([
-    HTMLElementType,
-    PropTypes.func,
-  ]),
-  /**
-   * Callback fired when the component requests to be closed.
-   * The `reason` parameter can optionally be used to control the response to `onClose`.
-   *
-   * @param {object} event The event source of the callback.
-   * @param {string} reason Can be: `"escapeKeyDown"`, `"backdropClick"`.
-   */
-  onClose: PropTypes.func,
-  /**
-   * If `true`, the component is shown.
-   */
-  open: PropTypes.bool.isRequired,
-  /**
-   * The system prop that allows defining system overrides as well as additional CSS styles.
-   */
-  sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-};
 
 export default Modal;
