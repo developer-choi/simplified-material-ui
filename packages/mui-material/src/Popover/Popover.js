473d299f9c233741c3406d398d2a1d606fd42e1e
Yu Jin Choe <forworkchoe@gmail.com>
2026-01-05 18:18:42 +0900

    [Popover 단순화 5/12] anchorOrigin/transformOrigin 숫자값 지원 제거

--- packages/mui-material/src/Popover/Popover.js
+++ packages/mui-material/src/Popover/Popover.js
@@ -20,13 +20,12 @@ import { getPopoverUtilityClass } from './popoverClasses';
 export function getOffsetTop(rect, vertical) {
   let offset = 0;
 
-  if (typeof vertical === 'number') {
-    offset = vertical;
-  } else if (vertical === 'center') {
+  if (vertical === 'center') {
     offset = rect.height / 2;
   } else if (vertical === 'bottom') {
     offset = rect.height;
   }
+  // 'top' or default: 0
 
   return offset;
 }
@@ -34,21 +33,18 @@ export function getOffsetTop(rect, vertical) {
 export function getOffsetLeft(rect, horizontal) {
   let offset = 0;
 
-  if (typeof horizontal === 'number') {
-    offset = horizontal;
-  } else if (horizontal === 'center') {
+  if (horizontal === 'center') {
     offset = rect.width / 2;
   } else if (horizontal === 'right') {
     offset = rect.width;
   }
+  // 'left' or default: 0
 
   return offset;
 }
 
 function getTransformOriginValue(transformOrigin) {
-  return [transformOrigin.horizontal, transformOrigin.vertical]
-    .map((n) => (typeof n === 'number' ? `${n}px` : n))
-    .join(' ');
+  return `${transformOrigin.horizontal} ${transformOrigin.vertical}`;
 }
 
 function resolveAnchorEl(anchorEl) {
