94ac12278b637029da02516c4e0dfb43349c962c
Yu Jin Choe <forworkchoe@gmail.com>
2026-01-19 13:36:16 +0900

    [FormLabel 단순화 5/5] PropTypes 제거

--- packages/mui-material/src/FormLabel/FormLabel.js
+++ packages/mui-material/src/FormLabel/FormLabel.js
@@ -1,6 +1,5 @@
 'use client';
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import formControlState from '../../../form/FormControl/formControlState';
 import useFormControl from '../../../form/FormControl/useFormControl';
 
@@ -60,66 +59,5 @@ const FormLabel = React.forwardRef(function FormLabel(props, ref) {
     </label>
   );
 });
-
-FormLabel.propTypes /* remove-proptypes */ = {
-  // ┌────────────────────────────── Warning ──────────────────────────────┐
-  // │ These PropTypes are generated from the TypeScript type definitions. │
-  // │    To update them, edit the d.ts file and run `pnpm proptypes`.     │
-  // └─────────────────────────────────────────────────────────────────────┘
-  /**
-   * The content of the component.
-   */
-  children: PropTypes.node,
-  /**
-   * Override or extend the styles applied to the component.
-   */
-  classes: PropTypes.object,
-  /**
-   * @ignore
-   */
-  className: PropTypes.string,
-  /**
-   * The color of the component.
-   * It supports both default and custom theme colors, which can be added as shown in the
-   * [palette customization guide](https://mui.com/material-ui/customization/palette/#custom-colors).
-   */
-  color: PropTypes /* @typescript-to-proptypes-ignore */.oneOfType([
-    PropTypes.oneOf(['error', 'info', 'primary', 'secondary', 'success', 'warning']),
-    PropTypes.string,
-  ]),
-  /**
-   * The component used for the root node.
-   * Either a string to use a HTML element or a component.
-   */
-  component: PropTypes.elementType,
-  /**
-   * If `true`, the label should be displayed in a disabled state.
-   */
-  disabled: PropTypes.bool,
-  /**
-   * If `true`, the label is displayed in an error state.
-   */
-  error: PropTypes.bool,
-  /**
-   * If `true`, the label should use filled classes key.
-   */
-  filled: PropTypes.bool,
-  /**
-   * If `true`, the input of this label is focused (used by `FormGroup` components).
-   */
-  focused: PropTypes.bool,
-  /**
-   * If `true`, the label will indicate that the `input` is required.
-   */
-  required: PropTypes.bool,
-  /**
-   * The system prop that allows defining system overrides as well as additional CSS styles.
-   */
-  sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-};
 
 export default FormLabel;
