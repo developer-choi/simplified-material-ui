2b776a57f08b2f12405ce4d6d0ca9b3de90df234
Yu Jin Choe <forworkchoe@gmail.com>
2026-01-22 17:02:23 +0900

    [IconButton 단순화 3/4] styled 시스템 제거

--- packages/mui-material/src/IconButton/IconButton.js
+++ packages/mui-material/src/IconButton/IconButton.js
@@ -3,152 +3,59 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import chainPropTypes from '@mui/utils/chainPropTypes';
 import { unstable_useId as useId } from '../utils';
-import { styled } from '../zero-styled';
-import memoTheme from '../utils/memoTheme';
-import createSimplePaletteValueFilter from '../utils/createSimplePaletteValueFilter';
 import ButtonBase from '../../../form/ButtonBase';
 import CircularProgress from '../CircularProgress';
-import capitalize from '../utils/capitalize';
 
-const IconButtonRoot = styled(ButtonBase, {
-  name: 'MuiIconButton',
-  slot: 'Root',
-  overridesResolver: (props, styles) => {
-    const { ownerState } = props;
+// 색상 매핑 (하드코딩)
+const colorMap = {
+  default: '#0000008a', // action.active
+  inherit: 'inherit',
+  primary: '#1976d2',
+  secondary: '#9c27b0',
+  error: '#d32f2f',
+  info: '#0288d1',
+  success: '#2e7d32',
+  warning: '#ed6c02',
+};
 
-    return [
-      styles.root,
-      ownerState.loading && styles.loading,
-      ownerState.color !== 'default' && styles[`color${capitalize(ownerState.color)}`],
-      ownerState.edge && styles[`edge${capitalize(ownerState.edge)}`],
-      styles[`size${capitalize(ownerState.size)}`],
-    ];
-  },
-})(
-  memoTheme(({ theme }) => ({
-    textAlign: 'center',
-    flex: '0 0 auto',
-    fontSize: theme.typography.pxToRem(24),
-    padding: 8,
-    borderRadius: '50%',
-    color: (theme.vars || theme).palette.action.active,
-    transition: theme.transitions.create('background-color', {
-      duration: theme.transitions.duration.shortest,
-    }),
-    variants: [
-      {
-        props: (props) => !props.disableRipple,
-        style: {
-          '--IconButton-hoverBg': theme.alpha(
-            (theme.vars || theme).palette.action.active,
-            (theme.vars || theme).palette.action.hoverOpacity,
-          ),
-          '&:hover': {
-            backgroundColor: 'var(--IconButton-hoverBg)',
-            // Reset on touch devices, it doesn't add specificity
-            '@media (hover: none)': {
-              backgroundColor: 'transparent',
-            },
-          },
-        },
-      },
-      {
-        props: { edge: 'start' },
-        style: {
-          marginLeft: -12,
-        },
-      },
-      {
-        props: { edge: 'start', size: 'small' },
-        style: {
-          marginLeft: -3,
-        },
-      },
-      {
-        props: { edge: 'end' },
-        style: {
-          marginRight: -12,
-        },
-      },
-      {
-        props: { edge: 'end', size: 'small' },
-        style: {
-          marginRight: -3,
-        },
-      },
-    ],
-  })),
-  memoTheme(({ theme }) => ({
-    variants: [
-      {
-        props: { color: 'inherit' },
-        style: {
-          color: 'inherit',
-        },
-      },
-      ...Object.entries(theme.palette)
-        .filter(createSimplePaletteValueFilter()) // check all the used fields in the style below
-        .map(([color]) => ({
-          props: { color },
-          style: {
-            color: (theme.vars || theme).palette[color].main,
-          },
-        })),
-      ...Object.entries(theme.palette)
-        .filter(createSimplePaletteValueFilter()) // check all the used fields in the style below
-        .map(([color]) => ({
-          props: { color },
-          style: {
-            '--IconButton-hoverBg': theme.alpha(
-              (theme.vars || theme).palette[color].main,
-              (theme.vars || theme).palette.action.hoverOpacity,
-            ),
-          },
-        })),
-      {
-        props: { size: 'small' },
-        style: {
-          padding: 5,
-          fontSize: theme.typography.pxToRem(18),
-        },
-      },
-      {
-        props: { size: 'large' },
-        style: {
-          padding: 12,
-          fontSize: theme.typography.pxToRem(28),
-        },
-      },
-      {
-        props: { disabled: true },
-        style: {
-          backgroundColor: 'transparent',
-          color: (theme.vars || theme).palette.action.disabled,
-        },
-      },
-      {
-        props: { loading: true },
-        style: {
-          color: 'transparent',
-        },
-      },
-    ],
-  })),
-);
+// 크기별 스타일
+const sizeStyles = {
+  small: { padding: 5, fontSize: '1.125rem' }, // 18px
+  medium: { padding: 8, fontSize: '1.5rem' }, // 24px
+  large: { padding: 12, fontSize: '1.75rem' }, // 28px
+};
 
-const IconButtonLoadingIndicator = styled('span', {
-  name: 'MuiIconButton',
-  slot: 'LoadingIndicator',
-})(({ theme }) => ({
-  display: 'none',
+// edge별 마진 (size에 따라 다름)
+const getEdgeMargin = (edge, size) => {
+  if (!edge) return {};
+  const isSmall = size === 'small';
+  if (edge === 'start') {
+    return { marginLeft: isSmall ? -3 : -12 };
+  }
+  if (edge === 'end') {
+    return { marginRight: isSmall ? -3 : -12 };
+  }
+  return {};
+};
+
+// 기본 스타일
+const baseStyle = {
+  textAlign: 'center',
+  flex: '0 0 auto',
+  borderRadius: '50%',
+  overflow: 'visible', // ripple이 보이도록
+  transition: 'background-color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+};
+
+// 로딩 인디케이터 스타일
+const loadingIndicatorStyle = {
   position: 'absolute',
   visibility: 'visible',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  color: (theme.vars || theme).palette.action.disabled,
-  variants: [{ props: { loading: true }, style: { display: 'flex' } }],
-}));
+  color: '#0000001f', // action.disabled
+};
 
 /**
  * Refer to the [Icons](/material-ui/icons/) section of the documentation
@@ -163,6 +70,7 @@ const IconButton = React.forwardRef(function IconButton(props, ref) {
     disabled = false,
     disableFocusRipple = false,
     size = 'medium',
+    style,
     id: idProp,
     loading = null,
     loadingIndicator: loadingIndicatorProp,
@@ -174,37 +82,43 @@ const IconButton = React.forwardRef(function IconButton(props, ref) {
     <CircularProgress aria-labelledby={loadingId} color="inherit" size={16} />
   );
 
-  const ownerState = {
-    ...props,
-    edge,
-    color,
-    disabled,
-    disableFocusRipple,
-    loading,
-    loadingIndicator,
-    size,
+  // 스타일 계산
+  const buttonColor = colorMap[color] || colorMap.default;
+  const sizeStyle = sizeStyles[size] || sizeStyles.medium;
+  const edgeMargin = getEdgeMargin(edge, size);
+
+  const computedStyle = {
+    ...baseStyle,
+    ...sizeStyle,
+    ...edgeMargin,
+    color: loading ? 'transparent' : buttonColor,
+    ...(disabled && {
+      backgroundColor: 'transparent',
+      color: '#0000001f', // action.disabled
+    }),
+    ...style,
   };
 
   return (
-    <IconButtonRoot
+    <ButtonBase
       id={loading ? loadingId : idProp}
       className={className}
+      style={computedStyle}
       centerRipple
       focusRipple={!disableFocusRipple}
       disabled={disabled || loading}
       ref={ref}
       {...other}
-      ownerState={ownerState}
     >
       {typeof loading === 'boolean' && (
         <span style={{ display: 'contents' }}>
-          <IconButtonLoadingIndicator ownerState={ownerState}>
+          <span style={{ ...loadingIndicatorStyle, display: loading ? 'flex' : 'none' }}>
             {loading && loadingIndicator}
-          </IconButtonLoadingIndicator>
+          </span>
         </span>
       )}
       {children}
-    </IconButtonRoot>
+    </ButtonBase>
   );
 });
 
