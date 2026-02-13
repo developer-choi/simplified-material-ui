e18e5100ee622b7c4f452bf1673c6e9424bdbbd4
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-13 22:52:06 +0900

    [CardActionArea 단순화 4/4] useUtilityClasses, composeClasses, PropTypes 제거

--- packages/mui-material/src/CardActionArea/CardActionArea.js
+++ packages/mui-material/src/CardActionArea/CardActionArea.js
@@ -1,21 +1,6 @@
 'use client';
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import composeClasses from '@mui/utils/composeClasses';
-import cardActionAreaClasses, { getCardActionAreaUtilityClass } from './cardActionAreaClasses';
 import ButtonBase from '../../../form/ButtonBase';
-
-const useUtilityClasses = (ownerState) => {
-  const { classes } = ownerState;
-
-  const slots = {
-    root: ['root'],
-    focusHighlight: ['focusHighlight'],
-  };
-
-  return composeClasses(slots, getCardActionAreaUtilityClass, classes);
-};
 
 const styles = {
   root: {
@@ -47,15 +32,12 @@ const CardActionArea = React.forwardRef(function CardActionArea(props, ref) {
     ...other
   } = props;
 
-  const ownerState = props;
-  const classes = useUtilityClasses(ownerState);
-
   return (
     <ButtonBase
       ref={ref}
-      className={clsx(classes.root, className)}
+      className={className}
       style={styles.root}
-      focusVisibleClassName={clsx(focusVisibleClassName, classes.focusVisible)}
+      focusVisibleClassName={focusVisibleClassName}
       onMouseEnter={(e) => {
         const highlight = e.currentTarget.querySelector('[data-focus-highlight]');
         if (highlight) highlight.style.opacity = '0.04';
@@ -75,40 +57,9 @@ const CardActionArea = React.forwardRef(function CardActionArea(props, ref) {
       {...other}
     >
       {children}
-      <span data-focus-highlight style={styles.focusHighlight} className={classes.focusHighlight} />
+      <span data-focus-highlight style={styles.focusHighlight} />
     </ButtonBase>
   );
 });
-
-CardActionArea.propTypes /* remove-proptypes */ = {
-  // ┌────────────────────────────── Warning ──────────────────────────────┐
-  // │ These PropTypes are generated from the TypeScript type definitions. │
-  // │    To update them, edit the d.ts file and run `pnpm proptypes`.     │
-  // └─────────────────────────────────────────────────────────────────────┘
-  /**
-   * The content of the component.
-   */
-  children: PropTypes.node,
-  /**
-   * Override or extend the styles applied to the component.
-   */
-  classes: PropTypes.object,
-  /**
-   * @ignore
-   */
-  className: PropTypes.string,
-  /**
-   * @ignore
-   */
-  focusVisibleClassName: PropTypes.string,
-  /**
-   * The system prop that allows defining system overrides as well as additional CSS styles.
-   */
-  sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-};
 
 export default CardActionArea;
