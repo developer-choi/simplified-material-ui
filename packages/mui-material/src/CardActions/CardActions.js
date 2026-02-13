b6a7942a81cc87adf1e2ea7b44491125fe3ee4ac
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-13 23:04:43 +0900

    [CardActions 단순화 3/3] useUtilityClasses, composeClasses, PropTypes 제거

--- packages/mui-material/src/CardActions/CardActions.js
+++ packages/mui-material/src/CardActions/CardActions.js
@@ -1,26 +1,8 @@
 'use client';
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import composeClasses from '@mui/utils/composeClasses';
-import { getCardActionsUtilityClass } from './cardActionsClasses';
-
-const useUtilityClasses = (ownerState) => {
-  const { classes, disableSpacing } = ownerState;
-
-  const slots = {
-    root: ['root', !disableSpacing && 'spacing'],
-  };
-
-  return composeClasses(slots, getCardActionsUtilityClass, classes);
-};
 
 const CardActions = React.forwardRef(function CardActions(props, ref) {
   const { disableSpacing = false, className, ...other } = props;
-
-  const ownerState = { ...props, disableSpacing };
-
-  const classes = useUtilityClasses(ownerState);
 
   const styles = {
     display: 'flex',
@@ -32,43 +14,11 @@ const CardActions = React.forwardRef(function CardActions(props, ref) {
   return (
     <div
       ref={ref}
-      className={clsx(classes.root, className)}
+      className={className}
       style={styles}
       {...other}
     />
   );
 });
-
-CardActions.propTypes /* remove-proptypes */ = {
-  // ┌────────────────────────────── Warning ──────────────────────────────┐
-  // │ These PropTypes are generated from the TypeScript type definitions. │
-  // │    To update them, edit the d.ts file and run `pnpm proptypes`.     │
-  // └─────────────────────────────────────────────────────────────────────┘
-  /**
-   * The content of the component.
-   */
-  children: PropTypes.node,
-  /**
-   * Override or extend the styles applied to the component.
-   */
-  classes: PropTypes.object,
-  /**
-   * @ignore
-   */
-  className: PropTypes.string,
-  /**
-   * If `true`, the actions do not have additional margin.
-   * @default false
-   */
-  disableSpacing: PropTypes.bool,
-  /**
-   * The system prop that allows defining system overrides as well as additional CSS styles.
-   */
-  sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-};
 
 export default CardActions;
