5c85d9b5c5e13eca9c3f9d535bf4447d2feea870
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-13 23:09:43 +0900

    [CardContent 단순화 3/3] useUtilityClasses, composeClasses, PropTypes 제거

--- packages/mui-material/src/CardContent/CardContent.js
+++ packages/mui-material/src/CardContent/CardContent.js
@@ -1,26 +1,8 @@
 'use client';
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import composeClasses from '@mui/utils/composeClasses';
-import { getCardContentUtilityClass } from './cardContentClasses';
-
-const useUtilityClasses = (ownerState) => {
-  const { classes } = ownerState;
-
-  const slots = {
-    root: ['root'],
-  };
-
-  return composeClasses(slots, getCardContentUtilityClass, classes);
-};
 
 const CardContent = React.forwardRef(function CardContent(props, ref) {
   const { className, component = 'div', ...other } = props;
-
-  const ownerState = { ...props, component };
-
-  const classes = useUtilityClasses(ownerState);
 
   const styles = {
     padding: 16,
@@ -31,43 +13,11 @@ const CardContent = React.forwardRef(function CardContent(props, ref) {
   return (
     <Component
       ref={ref}
-      className={clsx(classes.root, className)}
+      className={className}
       style={styles}
       {...other}
     />
   );
 });
-
-CardContent.propTypes /* remove-proptypes */ = {
-  // ┌────────────────────────────── Warning ──────────────────────────────┐
-  // │ These PropTypes are generated from the TypeScript type definitions. │
-  // │    To update them, edit the d.ts file and run `pnpm proptypes`.     │
-  // └─────────────────────────────────────────────────────────────────────┘
-  /**
-   * The content of the component.
-   */
-  children: PropTypes.node,
-  /**
-   * Override or extend the styles applied to the component.
-   */
-  classes: PropTypes.object,
-  /**
-   * @ignore
-   */
-  className: PropTypes.string,
-  /**
-   * The component used for the root node.
-   * Either a string to use a HTML element or a component.
-   */
-  component: PropTypes.elementType,
-  /**
-   * The system prop that allows defining system overrides as well as additional CSS styles.
-   */
-  sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-};
 
 export default CardContent;
