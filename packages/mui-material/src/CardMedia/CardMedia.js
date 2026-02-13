aad3ab1ba782f39f20f944031c6293e7173150fe
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-13 22:59:11 +0900

    [CardMedia 단순화 3/3] useUtilityClasses, composeClasses, PropTypes 제거

--- packages/mui-material/src/CardMedia/CardMedia.js
+++ packages/mui-material/src/CardMedia/CardMedia.js
@@ -1,10 +1,5 @@
 'use client';
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import chainPropTypes from '@mui/utils/chainPropTypes';
-import composeClasses from '@mui/utils/composeClasses';
-import { getCardMediaUtilityClass } from './cardMediaClasses';
 
 const MEDIA_COMPONENTS = ['video', 'audio', 'picture', 'iframe', 'img'];
 const IMAGE_COMPONENTS = ['picture', 'img'];
@@ -24,16 +19,6 @@ const styles = {
   },
 };
 
-const useUtilityClasses = (ownerState) => {
-  const { classes, isMediaComponent, isImageComponent } = ownerState;
-
-  const slots = {
-    root: ['root', isMediaComponent && 'media', isImageComponent && 'img'],
-  };
-
-  return composeClasses(slots, getCardMediaUtilityClass, classes);
-};
-
 const CardMedia = React.forwardRef(function CardMedia(props, ref) {
   const { children, className, component = 'div', image, src, style, ...other } = props;
 
@@ -48,21 +33,12 @@ const CardMedia = React.forwardRef(function CardMedia(props, ref) {
     ...style,
   };
 
-  const ownerState = {
-    ...props,
-    component,
-    isMediaComponent,
-    isImageComponent,
-  };
-
-  const classes = useUtilityClasses(ownerState);
-
   const Component = component;
 
   return (
     <Component
       ref={ref}
-      className={clsx(classes.root, className)}
+      className={className}
       role={!isMediaComponent && image ? 'img' : undefined}
       style={composedStyle}
       src={isMediaComponent ? image || src : undefined}
@@ -72,60 +48,5 @@ const CardMedia = React.forwardRef(function CardMedia(props, ref) {
     </Component>
   );
 });
-
-CardMedia.propTypes /* remove-proptypes */ = {
-  // ┌────────────────────────────── Warning ──────────────────────────────┐
-  // │ These PropTypes are generated from the TypeScript type definitions. │
-  // │    To update them, edit the d.ts file and run `pnpm proptypes`.     │
-  // └─────────────────────────────────────────────────────────────────────┘
-  /**
-   * The content of the component.
-   */
-  children: chainPropTypes(PropTypes.node, (props) => {
-    if (!props.children && !props.image && !props.src && !props.component) {
-      return new Error(
-        'MUI: Either `children`, `image`, `src` or `component` prop must be specified.',
-      );
-    }
-    return null;
-  }),
-  /**
-   * Override or extend the styles applied to the component.
-   */
-  classes: PropTypes.object,
-  /**
-   * @ignore
-   */
-  className: PropTypes.string,
-  /**
-   * The component used for the root node.
-   * Either a string to use a HTML element or a component.
-   */
-  component: PropTypes.elementType,
-  /**
-   * Image to be displayed as a background image.
-   * Either `image` or `src` prop must be specified.
-   * Note that caller must specify height otherwise the image will not be visible.
-   */
-  image: PropTypes.string,
-  /**
-   * An alias for `image` property.
-   * Available only with media components.
-   * Media components: `video`, `audio`, `picture`, `iframe`, `img`.
-   */
-  src: PropTypes.string,
-  /**
-   * @ignore
-   */
-  style: PropTypes.object,
-  /**
-   * The system prop that allows defining system overrides as well as additional CSS styles.
-   */
-  sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-};
 
 export default CardMedia;
