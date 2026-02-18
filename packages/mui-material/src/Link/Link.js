364cfd9a49329a7ae9d4bf6bde421af9d62faa33
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-18 14:40:28 +0900

    [Link 단순화 8/8] styled(Typography) → <a> 인라인 스타일 변환

--- packages/mui-material/src/Link/Link.js
+++ packages/mui-material/src/Link/Link.js
@@ -1,65 +1,12 @@
 'use client';
 import * as React from 'react';
 import isFocusVisible from '@mui/utils/isFocusVisible';
-import capitalize from '../utils/capitalize';
-import { styled } from '../zero-styled';
-import memoTheme from '../utils/memoTheme';
-import Typography from '../Typography';
 
-
-const LinkRoot = styled(Typography, {
-  name: 'MuiLink',
-  slot: 'Root',
-  overridesResolver: (props, styles) => {
-    const { ownerState } = props;
-
-    return [
-      styles.root,
-      styles[`underline${capitalize(ownerState.underline)}`],
-      ownerState.component === 'button' && styles.button,
-    ];
-  },
-})(
-  memoTheme(({ theme }) => {
-    return {
-      variants: [
-        {
-          props: {
-            underline: 'none',
-          },
-          style: {
-            textDecoration: 'none',
-          },
-        },
-        {
-          props: {
-            underline: 'hover',
-          },
-          style: {
-            textDecoration: 'none',
-            '&:hover': {
-              textDecoration: 'underline',
-            },
-          },
-        },
-        {
-          props: {
-            underline: 'always',
-          },
-          style: {
-            textDecoration: 'underline',
-            '&:hover': {
-              textDecorationColor: 'inherit',
-            },
-          },
-        },
-      ],
-    };
-  }),
-);
+const linkStyles = `.MuiLink-hover:hover { text-decoration: underline; }`;
 
 function Link(props) {
   const {
+    children,
     underline = 'always',
     ...other
   } = props;
@@ -76,19 +23,23 @@ function Link(props) {
     }
   };
 
-  const ownerState = {
-    ...props,
-    focusVisible,
-    underline,
-  };
-
   return (
-    <LinkRoot
-      onBlur={handleBlur}
-      onFocus={handleFocus}
-      ownerState={ownerState}
-      {...other}
-    />
+    <>
+      <style>{linkStyles}</style>
+      <a
+        onBlur={handleBlur}
+        onFocus={handleFocus}
+        className={underline === 'hover' ? 'MuiLink-hover' : undefined}
+        style={{
+          color: '#1976d2',
+          textDecoration: underline === 'always' ? 'underline' : 'none',
+          outline: focusVisible ? 'auto' : 0,
+        }}
+        {...other}
+      >
+        {children}
+      </a>
+    </>
   );
 }
 
