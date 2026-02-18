ecdcc033a8301e3ef0da5981f4e8e10e4fad6443
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-18 14:39:59 +0900

    [Link 단순화 5/8] component/variant/onBlur/onFocus props 제거

--- packages/mui-material/src/Link/Link.js
+++ packages/mui-material/src/Link/Link.js
@@ -5,7 +5,6 @@ import capitalize from '../utils/capitalize';
 import { styled } from '../zero-styled';
 import memoTheme from '../utils/memoTheme';
 import Typography from '../Typography';
-import linkClasses from './linkClasses';
 
 
 const LinkRoot = styled(Typography, {
@@ -54,33 +53,6 @@ const LinkRoot = styled(Typography, {
             },
           },
         },
-        {
-          props: {
-            component: 'button',
-          },
-          style: {
-            position: 'relative',
-            WebkitTapHighlightColor: 'transparent',
-            backgroundColor: 'transparent', // Reset default value
-            // We disable the focus ring for mouse, touch and keyboard users.
-            outline: 0,
-            border: 0,
-            margin: 0, // Remove the margin in Safari
-            borderRadius: 0,
-            padding: 0, // Remove the padding in Firefox
-            cursor: 'pointer',
-            userSelect: 'none',
-            verticalAlign: 'middle',
-            MozAppearance: 'none', // Reset
-            WebkitAppearance: 'none', // Reset
-            '&::-moz-focus-inner': {
-              borderStyle: 'none', // Remove Firefox dotted outline.
-            },
-            [`&.${linkClasses.focusVisible}`]: {
-              outline: 'auto',
-            },
-          },
-        },
       ],
     };
   }),
@@ -88,11 +60,7 @@ const LinkRoot = styled(Typography, {
 
 const Link = React.forwardRef(function Link(props, ref) {
   const {
-    component = 'a',
-    onBlur,
-    onFocus,
     underline = 'always',
-    variant = 'inherit',
     sx,
     ...other
   } = props;
@@ -102,35 +70,25 @@ const Link = React.forwardRef(function Link(props, ref) {
     if (!isFocusVisible(event.target)) {
       setFocusVisible(false);
     }
-    if (onBlur) {
-      onBlur(event);
-    }
   };
   const handleFocus = (event) => {
     if (isFocusVisible(event.target)) {
       setFocusVisible(true);
     }
-    if (onFocus) {
-      onFocus(event);
-    }
   };
 
   const ownerState = {
     ...props,
-    component,
     focusVisible,
     underline,
-    variant,
   };
 
   return (
     <LinkRoot
-      component={component}
       onBlur={handleBlur}
       onFocus={handleFocus}
       ref={ref}
       ownerState={ownerState}
-      variant={variant}
       {...other}
       sx={Array.isArray(sx) ? sx : [sx]}
     />
