60068cca12b0f6eb3a2c37afa85518cbfe36021e
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-20 08:35:39 +0900

    [Grid 단순화 4/5] styled → inline styles

--- packages/mui-material/src/Grid/Grid.js
+++ packages/mui-material/src/Grid/Grid.js
@@ -1,50 +1,6 @@
 'use client';
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import { styled } from '../styles';
-
-const GridRoot = styled('div')(({ ownerState }) => {
-  const gap = typeof ownerState.spacing === 'string'
-    ? ownerState.spacing
-    : `${ownerState.spacing * 8}px`;
-
-  return {
-    minWidth: 0,
-    boxSizing: 'border-box',
-    ...(ownerState.container && {
-      display: 'flex',
-      flexWrap: ownerState.wrap,
-      flexDirection: ownerState.direction,
-      '--Grid-columns': ownerState.columns,
-      '--Grid-gap': gap,
-      gap: `var(--Grid-gap)`,
-    }),
-    ...(ownerState.size === 'grow' && {
-      flexBasis: 0,
-      flexGrow: 1,
-      maxWidth: '100%',
-    }),
-    ...(ownerState.size === 'auto' && {
-      flexBasis: 'auto',
-      flexGrow: 0,
-      flexShrink: 0,
-      maxWidth: 'none',
-      width: 'auto',
-    }),
-    ...(typeof ownerState.size === 'number' && {
-      flexGrow: 0,
-      flexBasis: 'auto',
-      width: `calc(100% * ${ownerState.size} / var(--Grid-columns) - (var(--Grid-columns) - ${ownerState.size}) * var(--Grid-gap) / var(--Grid-columns))`,
-    }),
-    ...(ownerState.offset === 'auto' && { marginLeft: 'auto' }),
-    ...(typeof ownerState.offset === 'number' && ownerState.offset === 0 && {
-      marginLeft: '0px',
-    }),
-    ...(typeof ownerState.offset === 'number' && ownerState.offset > 0 && {
-      marginLeft: `calc(100% * ${ownerState.offset} / var(--Grid-columns) + var(--Grid-gap) * ${ownerState.offset} / var(--Grid-columns))`,
-    }),
-  };
-});
 
 const Grid = React.forwardRef(function Grid(props, ref) {
   const {
@@ -60,25 +16,47 @@ const Grid = React.forwardRef(function Grid(props, ref) {
     ...other
   } = props;
 
-  const ownerState = {
-    columns,
-    container,
-    direction,
-    wrap,
-    spacing,
-    size,
-    offset,
+  const gap = typeof spacing === 'string' ? spacing : `${spacing * 8}px`;
+
+  const style = {
+    minWidth: 0,
+    boxSizing: 'border-box',
+    ...(container && {
+      display: 'flex',
+      flexWrap: wrap,
+      flexDirection: direction,
+      '--Grid-columns': columns,
+      '--Grid-gap': gap,
+      gap: `var(--Grid-gap)`,
+    }),
+    ...(size === 'grow' && {
+      flexBasis: 0,
+      flexGrow: 1,
+      maxWidth: '100%',
+    }),
+    ...(size === 'auto' && {
+      flexBasis: 'auto',
+      flexGrow: 0,
+      flexShrink: 0,
+      maxWidth: 'none',
+      width: 'auto',
+    }),
+    ...(typeof size === 'number' && {
+      flexGrow: 0,
+      flexBasis: 'auto',
+      width: `calc(100% * ${size} / var(--Grid-columns) - (var(--Grid-columns) - ${size}) * var(--Grid-gap) / var(--Grid-columns))`,
+    }),
+    ...(offset === 'auto' && { marginLeft: 'auto' }),
+    ...(typeof offset === 'number' && offset === 0 && { marginLeft: '0px' }),
+    ...(typeof offset === 'number' && offset > 0 && {
+      marginLeft: `calc(100% * ${offset} / var(--Grid-columns) + var(--Grid-gap) * ${offset} / var(--Grid-columns))`,
+    }),
   };
 
   return (
-    <GridRoot
-      ref={ref}
-      ownerState={ownerState}
-      className={className}
-      {...other}
-    >
+    <div ref={ref} className={className} style={style} {...other}>
       {children}
-    </GridRoot>
+    </div>
   );
 });
 
@@ -90,11 +68,6 @@ Grid.propTypes /* remove-proptypes */ = {
   offset: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   size: PropTypes.oneOfType([PropTypes.string, PropTypes.bool, PropTypes.number]),
   spacing: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
   wrap: PropTypes.oneOf(['nowrap', 'wrap-reverse', 'wrap']),
 };
 
