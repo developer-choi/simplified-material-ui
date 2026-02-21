9e32002120b54c8612ad0a7497797f295030601a
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-22 01:27:27 +0900

    [StepConnector 1/3] styled → 인라인 스타일, memoTheme 제거
    - StepConnectorRoot, StepConnectorLine styled 컴포넌트 제거
    - div + span + inline style로 대체
    - active || completed 상태로 선 색상 분기 (#1976d2 / #bdbdbd)
    - alternativeLabel 관련 스타일 제거 (Stepper Context에서 이미 제거됨)
    - styled, memoTheme import 제거

--- packages/mui-material/src/StepConnector/StepConnector.js
+++ packages/mui-material/src/StepConnector/StepConnector.js
@@ -4,8 +4,6 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import composeClasses from '@mui/utils/composeClasses';
 import capitalize from '../utils/capitalize';
-import { styled } from '../zero-styled';
-import memoTheme from '../utils/memoTheme';
 import { useDefaultProps } from '../DefaultPropsProvider';
 import StepperContext from '../Stepper/StepperContext';
 import StepContext from '../Step/StepContext';
@@ -29,95 +27,41 @@ const useUtilityClasses = (ownerState) => {
   return composeClasses(slots, getStepConnectorUtilityClass, classes);
 };
 
-const StepConnectorRoot = styled('div', {
-  name: 'MuiStepConnector',
-  slot: 'Root',
-  overridesResolver: (props, styles) => {
-    const { ownerState } = props;
-
-    return [
-      styles.root,
-      styles[ownerState.orientation],
-      ownerState.alternativeLabel && styles.alternativeLabel,
-      ownerState.completed && styles.completed,
-    ];
-  },
-})({
-  flex: '1 1 auto',
-  variants: [
-    {
-      props: { orientation: 'vertical' },
-      style: {
-        marginLeft: 12, // half icon
-      },
-    },
-    {
-      props: { alternativeLabel: true },
-      style: {
-        position: 'absolute',
-        top: 8 + 4,
-        left: 'calc(-50% + 20px)',
-        right: 'calc(50% + 20px)',
-      },
-    },
-  ],
-});
-
-const StepConnectorLine = styled('span', {
-  name: 'MuiStepConnector',
-  slot: 'Line',
-  overridesResolver: (props, styles) => {
-    const { ownerState } = props;
-
-    return [styles.line, styles[`line${capitalize(ownerState.orientation)}`]];
-  },
-})(
-  memoTheme(({ theme }) => {
-    const borderColor =
-      theme.palette.mode === 'light' ? theme.palette.grey[400] : theme.palette.grey[600];
-    return {
-      display: 'block',
-      borderColor: theme.vars ? theme.vars.palette.StepConnector.border : borderColor,
-      variants: [
-        {
-          props: { orientation: 'horizontal' },
-          style: {
-            borderTopStyle: 'solid',
-            borderTopWidth: 1,
-          },
-        },
-        {
-          props: { orientation: 'vertical' },
-          style: {
-            borderLeftStyle: 'solid',
-            borderLeftWidth: 1,
-            minHeight: 24,
-          },
-        },
-      ],
-    };
-  }),
-);
-
 const StepConnector = React.forwardRef(function StepConnector(inProps, ref) {
   const props = useDefaultProps({ props: inProps, name: 'MuiStepConnector' });
-  const { className, ...other } = props;
+  const { className, style, ...other } = props;
 
-  const { alternativeLabel, orientation = 'horizontal' } = React.useContext(StepperContext);
+  const { orientation = 'horizontal' } = React.useContext(StepperContext);
   const { active, disabled, completed } = React.useContext(StepContext);
 
-  const ownerState = { ...props, alternativeLabel, orientation, active, completed, disabled };
+  const ownerState = { ...props, orientation, active, completed, disabled };
   const classes = useUtilityClasses(ownerState);
 
+  const lineColor = (active || completed) ? '#1976d2' : '#bdbdbd';
+
   return (
-    <StepConnectorRoot
+    <div
       className={clsx(classes.root, className)}
       ref={ref}
-      ownerState={ownerState}
+      style={{
+        flex: '1 1 auto',
+        ...(orientation === 'vertical' && { marginLeft: 12 }),
+        ...style,
+      }}
       {...other}
     >
-      <StepConnectorLine className={classes.line} ownerState={ownerState} />
-    </StepConnectorRoot>
+      <span
+        className={classes.line}
+        style={{
+          display: 'block',
+          borderColor: lineColor,
+          ...(orientation === 'horizontal'
+            ? { borderTopStyle: 'solid', borderTopWidth: 1 }
+            : { borderLeftStyle: 'solid', borderLeftWidth: 1, minHeight: 24 }
+          ),
+        }}
+      />
+    </div>
   );
 });
 
