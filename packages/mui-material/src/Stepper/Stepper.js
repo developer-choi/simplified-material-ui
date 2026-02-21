ad823e54e6fbbbe99311e3fefefec158b05ac7a1
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-21 22:54:18 +0900

    [Stepper 3/3] useDefaultProps, PropTypes, ownerState 제거
    - useDefaultProps 제거, inProps → props로 직접 비구조화
    - PropTypes 블록 전체 제거
    - PropTypes, useDefaultProps import 제거

--- packages/mui-material/src/Stepper/Stepper.js
+++ packages/mui-material/src/Stepper/Stepper.js
@@ -1,11 +1,8 @@
 'use client';
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import { useDefaultProps } from '../DefaultPropsProvider';
 import StepperContext from './StepperContext';
 
-const Stepper = React.forwardRef(function Stepper(inProps, ref) {
-  const props = useDefaultProps({ props: inProps, name: 'MuiStepper' });
+const Stepper = React.forwardRef(function Stepper(props, ref) {
   const {
     activeStep = 0,
     children,
@@ -46,64 +43,5 @@ const Stepper = React.forwardRef(function Stepper(inProps, ref) {
     </StepperContext.Provider>
   );
 });
-
-Stepper.propTypes /* remove-proptypes */ = {
-  // ┌────────────────────────────── Warning ──────────────────────────────┐
-  // │ These PropTypes are generated from the TypeScript type definitions. │
-  // │    To update them, edit the d.ts file and run `pnpm proptypes`.     │
-  // └─────────────────────────────────────────────────────────────────────┘
-  /**
-   * Set the active step (zero based index).
-   * Set to -1 to disable all the steps.
-   * @default 0
-   */
-  activeStep: integerPropType,
-  /**
-   * If set to 'true' and orientation is horizontal,
-   * then the step label will be positioned under the icon.
-   * @default false
-   */
-  alternativeLabel: PropTypes.bool,
-  /**
-   * Two or more `<Step />` components.
-   */
-  children: PropTypes.node,
-  /**
-   * Override or extend the styles applied to the component.
-   */
-  classes: PropTypes.object,
-  /**
-   * @ignore
-   */
-  className: PropTypes.string,
-  /**
-   * The component used for the root node.
-   * Either a string to use a HTML element or a component.
-   */
-  component: PropTypes.elementType,
-  /**
-   * An element to be placed between each step.
-   * @default <StepConnector />
-   */
-  connector: PropTypes.element,
-  /**
-   * If set the `Stepper` will not assist in controlling steps for linear flow.
-   * @default false
-   */
-  nonLinear: PropTypes.bool,
-  /**
-   * The component orientation (layout flow direction).
-   * @default 'horizontal'
-   */
-  orientation: PropTypes.oneOf(['horizontal', 'vertical']),
-  /**
-   * The system prop that allows defining system overrides as well as additional CSS styles.
-   */
-  sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-};
 
 export default Stepper;
