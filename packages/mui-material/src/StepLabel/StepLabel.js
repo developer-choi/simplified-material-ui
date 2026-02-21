9259540fc97caef55101d979c2dbd250daf4df26
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-22 01:46:56 +0900

    [StepLabel 3/3] useDefaultProps, PropTypes, ownerState 제거
    - useDefaultProps 제거, inProps → props 직접 비구조화
    - PropTypes 블록 전체 제거
    - alternativeLabel context 소비 제거 (Stepper에서 이미 제거됨)
    - StepLabel.muiName 제거
    - PropTypes, useDefaultProps import 제거

--- packages/mui-material/src/StepLabel/StepLabel.js
+++ packages/mui-material/src/StepLabel/StepLabel.js
@@ -1,13 +1,10 @@
 'use client';
-import PropTypes from 'prop-types';
 import * as React from 'react';
 import StepContext from '../Step/StepContext';
 import StepIcon from '../StepIcon';
 import StepperContext from '../Stepper/StepperContext';
-import { useDefaultProps } from '../DefaultPropsProvider';
 
-const StepLabel = React.forwardRef(function StepLabel(inProps, ref) {
-  const props = useDefaultProps({ props: inProps, name: 'MuiStepLabel' });
+const StepLabel = React.forwardRef(function StepLabel(props, ref) {
   const {
     children,
     className,
@@ -18,7 +15,7 @@ const StepLabel = React.forwardRef(function StepLabel(inProps, ref) {
     ...other
   } = props;
 
-  const { alternativeLabel, orientation } = React.useContext(StepperContext);
+  const { orientation } = React.useContext(StepperContext);
   const { active, disabled, completed, icon: iconContext } = React.useContext(StepContext);
   const icon = iconProp || iconContext;
 
@@ -64,83 +61,5 @@ const StepLabel = React.forwardRef(function StepLabel(inProps, ref) {
     </span>
   );
 });
-
-StepLabel.propTypes /* remove-proptypes */ = {
-  // ┌────────────────────────────── Warning ──────────────────────────────┐
-  // │ These PropTypes are generated from the TypeScript type definitions. │
-  // │    To update them, edit the d.ts file and run `pnpm proptypes`.     │
-  // └─────────────────────────────────────────────────────────────────────┘
-  /**
-   * In most cases will simply be a string containing a title for the label.
-   */
-  children: PropTypes.node,
-  /**
-   * Override or extend the styles applied to the component.
-   */
-  classes: PropTypes.object,
-  /**
-   * @ignore
-   */
-  className: PropTypes.string,
-  /**
-   * The props used for each slot inside.
-   * @default {}
-   * @deprecated use the `slotProps` prop instead. This prop will be removed in a future major release. See [Migrating from deprecated APIs](https://mui.com/material-ui/migration/migrating-from-deprecated-apis/) for more details.
-   */
-  componentsProps: PropTypes.shape({
-    label: PropTypes.object,
-  }),
-  /**
-   * If `true`, the step is marked as failed.
-   * @default false
-   */
-  error: PropTypes.bool,
-  /**
-   * Override the default label of the step icon.
-   */
-  icon: PropTypes.node,
-  /**
-   * The optional node to display.
-   */
-  optional: PropTypes.node,
-  /**
-   * The props used for each slot inside.
-   * @default {}
-   */
-  slotProps: PropTypes.shape({
-    label: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-    root: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-    stepIcon: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-  }),
-  /**
-   * The components used for each slot inside.
-   * @default {}
-   */
-  slots: PropTypes.shape({
-    label: PropTypes.elementType,
-    root: PropTypes.elementType,
-    stepIcon: PropTypes.elementType,
-  }),
-  /**
-   * The component to render in place of the [`StepIcon`](https://mui.com/material-ui/api/step-icon/).
-   * @deprecated Use `slots.stepIcon` instead. This prop will be removed in a future major release. See [Migrating from deprecated APIs](/material-ui/migration/migrating-from-deprecated-apis/) for more details.
-   */
-  StepIconComponent: PropTypes.elementType,
-  /**
-   * Props applied to the [`StepIcon`](https://mui.com/material-ui/api/step-icon/) element.
-   * @deprecated Use `slotProps.stepIcon` instead. This prop will be removed in a future major release. See [Migrating from deprecated APIs](/material-ui/migration/migrating-from-deprecated-apis/) for more details.
-   */
-  StepIconProps: PropTypes.object,
-  /**
-   * The system prop that allows defining system overrides as well as additional CSS styles.
-   */
-  sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-};
-
-StepLabel.muiName = 'StepLabel';
 
 export default StepLabel;
