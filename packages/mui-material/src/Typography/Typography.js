8e474a025ab71d53718c4c8761afaaf6a61df15e
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-22 15:52:31 +0900

    [Typography 3/3] useDefaultProps, PropTypes, ownerState 제거

--- packages/mui-material/src/Typography/Typography.js
+++ packages/mui-material/src/Typography/Typography.js
@@ -1,7 +1,5 @@
 'use client';
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import { useDefaultProps } from '../DefaultPropsProvider';
 
 const defaultVariantMapping = {
   h1: 'h1',
@@ -34,9 +32,7 @@ const typographyStyles = {
   inherit:   { font: 'inherit', lineHeight: 'inherit', letterSpacing: 'inherit' },
 };
 
-const Typography = React.forwardRef(function Typography(inProps, ref) {
-  const props = useDefaultProps({ props: inProps, name: 'MuiTypography' });
-
+const Typography = React.forwardRef(function Typography(props, ref) {
   const {
     align = 'inherit',
     children,
@@ -49,15 +45,6 @@ const Typography = React.forwardRef(function Typography(inProps, ref) {
     style,
     ...other
   } = props;
-
-  const ownerState = {
-    ...props,
-    align,
-    gutterBottom,
-    noWrap,
-    variant,
-    variantMapping,
-  };
 
   const Component = component || variantMapping[variant] || defaultVariantMapping[variant] || 'span';
 
@@ -79,60 +66,5 @@ const Typography = React.forwardRef(function Typography(inProps, ref) {
     </Component>
   );
 });
-
-Typography.propTypes /* remove-proptypes */ = {
-  // ┌────────────────────────────── Warning ──────────────────────────────┐
-  // │ These PropTypes are generated from the TypeScript type definitions. │
-  // │    To update them, edit the d.ts file and run `pnpm proptypes`.     │
-  // └─────────────────────────────────────────────────────────────────────┘
-  /**
-   * Set the text-align on the component.
-   * @default 'inherit'
-   */
-  align: PropTypes.oneOf(['center', 'inherit', 'justify', 'left', 'right']),
-  /**
-   * The content of the component.
-   */
-  children: PropTypes.node,
-  /**
-   * Override or extend the styles applied to the component.
-   */
-  classes: PropTypes.object,
-  /**
-   * @ignore
-   */
-  className: PropTypes.string,
-  /**
-   * The component used for the root node.
-   */
-  component: PropTypes.elementType,
-  /**
-   * If `true`, the text will have a bottom margin.
-   * @default false
-   */
-  gutterBottom: PropTypes.bool,
-  /**
-   * If `true`, the text will not wrap, but instead will truncate with a text overflow ellipsis.
-   * @default false
-   */
-  noWrap: PropTypes.bool,
-  /**
-   * @ignore
-   */
-  style: PropTypes.object,
-  /**
-   * Applies the theme typography styles.
-   * @default 'body1'
-   */
-  variant: PropTypes.oneOf([
-    'body1', 'body2', 'button', 'caption',
-    'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-    'inherit', 'overline', 'subtitle1', 'subtitle2',
-  ]),
-  /**
-   * The component maps the variant prop to a range of different HTML element types.
-   */
-  variantMapping: PropTypes.object,
-};
 
 export default Typography;
