070b68cd0ca35e1cfccaf72f8c0feac00427ee3b
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-22 12:56:52 +0900

    [StepButton 1/3] styled(ButtonBase) → button 인라인 스타일, isMuiElement 제거

--- packages/mui-material/src/StepButton/StepButton.js
+++ packages/mui-material/src/StepButton/StepButton.js
@@ -3,61 +3,25 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import composeClasses from '@mui/utils/composeClasses';
-import { styled } from '../zero-styled';
 import { useDefaultProps } from '../DefaultPropsProvider';
-import ButtonBase from '../../../form/ButtonBase';
 import StepLabel from '../StepLabel';
-import isMuiElement from '../utils/isMuiElement';
 import StepperContext from '../Stepper/StepperContext';
 import StepContext from '../Step/StepContext';
-import stepButtonClasses, { getStepButtonUtilityClass } from './stepButtonClasses';
+import { getStepButtonUtilityClass } from './stepButtonClasses';
 
 const useUtilityClasses = (ownerState) => {
   const { classes, orientation } = ownerState;
 
   const slots = {
     root: ['root', orientation],
-    touchRipple: ['touchRipple'],
   };
 
   return composeClasses(slots, getStepButtonUtilityClass, classes);
 };
 
-const StepButtonRoot = styled(ButtonBase, {
-  name: 'MuiStepButton',
-  slot: 'Root',
-  overridesResolver: (props, styles) => {
-    const { ownerState } = props;
-
-    return [
-      { [`& .${stepButtonClasses.touchRipple}`]: styles.touchRipple },
-      styles.root,
-      styles[ownerState.orientation],
-    ];
-  },
-})({
-  width: '100%',
-  padding: '24px 16px',
-  margin: '-24px -16px',
-  boxSizing: 'content-box',
-  [`& .${stepButtonClasses.touchRipple}`]: {
-    color: 'rgba(0, 0, 0, 0.3)',
-  },
-  variants: [
-    {
-      props: { orientation: 'vertical' },
-      style: {
-        justifyContent: 'flex-start',
-        padding: '8px',
-        margin: '-8px',
-      },
-    },
-  ],
-});
-
 const StepButton = React.forwardRef(function StepButton(inProps, ref) {
   const props = useDefaultProps({ props: inProps, name: 'MuiStepButton' });
-  const { children, className, icon, optional, ...other } = props;
+  const { children, className, icon, optional, style, ...other } = props;
 
   const { disabled, active } = React.useContext(StepContext);
   const { orientation } = React.useContext(StepperContext);
@@ -66,30 +30,33 @@ const StepButton = React.forwardRef(function StepButton(inProps, ref) {
 
   const classes = useUtilityClasses(ownerState);
 
-  const childProps = {
-    icon,
-    optional,
-  };
-
-  const child = isMuiElement(children, ['StepLabel']) ? (
-    React.cloneElement(children, childProps)
-  ) : (
-    <StepLabel {...childProps}>{children}</StepLabel>
-  );
-
   return (
-    <StepButtonRoot
-      focusRipple
-      disabled={disabled}
-      TouchRippleProps={{ className: classes.touchRipple }}
+    <button
       className={clsx(classes.root, className)}
       ref={ref}
-      ownerState={ownerState}
+      disabled={disabled}
       aria-current={active ? 'step' : undefined}
+      style={{
+        display: 'inline-flex',
+        alignItems: 'center',
+        justifyContent: orientation === 'vertical' ? 'flex-start' : 'center',
+        background: 'none',
+        border: 0,
+        cursor: 'inherit',
+        outline: 0,
+        padding: 0,
+        textDecoration: 'none',
+        width: '100%',
+        boxSizing: 'content-box',
+        ...(orientation === 'vertical'
+          ? { padding: '8px', margin: '-8px' }
+          : { padding: '24px 16px', margin: '-24px -16px' }),
+        ...style,
+      }}
       {...other}
     >
-      {child}
-    </StepButtonRoot>
+      <StepLabel icon={icon} optional={optional}>{children}</StepLabel>
+    </button>
   );
 });
 
