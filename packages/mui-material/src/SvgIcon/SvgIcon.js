3541bb4d3afb5d7b833d34a6b37f9f3a90c96fe1
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-22 15:35:08 +0900

    [SvgIcon 3/3] useDefaultProps, PropTypes, ownerState, muiName 제거

--- packages/mui-material/src/SvgIcon/SvgIcon.js
+++ packages/mui-material/src/SvgIcon/SvgIcon.js
@@ -1,7 +1,5 @@
 'use client';
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import { useDefaultProps } from '../DefaultPropsProvider';
 
 const fontSizeMap = {
   inherit: 'inherit',
@@ -10,8 +8,7 @@ const fontSizeMap = {
   large: '2.1875rem',
 };
 
-const SvgIcon = React.forwardRef(function SvgIcon(inProps, ref) {
-  const props = useDefaultProps({ props: inProps, name: 'MuiSvgIcon' });
+const SvgIcon = React.forwardRef(function SvgIcon(props, ref) {
   const {
     children,
     className,
@@ -25,14 +22,6 @@ const SvgIcon = React.forwardRef(function SvgIcon(inProps, ref) {
   } = props;
 
   const hasSvgAsChild = React.isValidElement(children) && children.type === 'svg';
-
-  const ownerState = {
-    ...props,
-    fontSize,
-    inheritViewBox,
-    viewBox,
-    hasSvgAsChild,
-  };
 
   return (
     <svg
@@ -61,63 +50,5 @@ const SvgIcon = React.forwardRef(function SvgIcon(inProps, ref) {
     </svg>
   );
 });
-
-SvgIcon.propTypes /* remove-proptypes */ = {
-  // ┌────────────────────────────── Warning ──────────────────────────────┐
-  // │ These PropTypes are generated from the TypeScript type definitions. │
-  // │    To update them, edit the d.ts file and run `pnpm proptypes`.     │
-  // └─────────────────────────────────────────────────────────────────────┘
-  /**
-   * Node passed into the SVG element.
-   */
-  children: PropTypes.node,
-  /**
-   * Override or extend the styles applied to the component.
-   */
-  classes: PropTypes.object,
-  /**
-   * @ignore
-   */
-  className: PropTypes.string,
-  /**
-   * The fontSize applied to the icon. Defaults to 24px, but can be configure to inherit font size.
-   * @default 'medium'
-   */
-  fontSize: PropTypes /* @typescript-to-proptypes-ignore */.oneOfType([
-    PropTypes.oneOf(['inherit', 'large', 'medium', 'small']),
-    PropTypes.string,
-  ]),
-  /**
-   * Applies a color attribute to the SVG element.
-   */
-  htmlColor: PropTypes.string,
-  /**
-   * If `true`, the root node will inherit the custom `component`'s viewBox and the `viewBox`
-   * prop will be ignored.
-   * @default false
-   */
-  inheritViewBox: PropTypes.bool,
-  /**
-   * The shape-rendering attribute.
-   */
-  shapeRendering: PropTypes.string,
-  /**
-   * The system prop that allows defining system overrides as well as additional CSS styles.
-   */
-  sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-  /**
-   * Provides a human-readable title for the element that contains it.
-   */
-  titleAccess: PropTypes.string,
-  /**
-   * Allows you to redefine what the coordinates without units mean inside an SVG element.
-   * @default '0 0 24 24'
-   */
-  viewBox: PropTypes.string,
-};
 
 export default SvgIcon;
